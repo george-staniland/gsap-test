efead66115d9c4a1225298555ca9755aa2716cda
George Staniland <georgestaniland@gmail.com>
2024-07-09 14:35:35 +1200

    add tl

--- main.js
+++ main.js
@@ -46,3 +46,34 @@ addEventListener("load", (event) => {
 });
 
 
+// With TimeLine
+// addEventListener("load", (event) => {
+//   gsap.registerPlugin(ScrollTrigger) 
+
+//   const tl = gsap.timeline({
+//     scrollTrigger: {
+//       trigger: ".section-2",
+//       scrub: true,
+//       start: "top bottom",
+//       end: "top top",
+//       markers: true,
+//     },
+//   })
+//   .to(".section-2 h1", {
+//     x: 0,
+//     transformOrigin: "center center", 
+//     ease: "none",
+//   })
+//   .to(".section-2 .circle", {
+//     height: '90vh',
+//     width: '90vh',
+//     borderColor: 'rgba(0, 0, 0, 1)',
+//     transformOrigin: "center center", 
+//     duration: 1,
+//     ease: "elastic",
+//   }, "-=0.5" )
+
+// });
+
+
+
