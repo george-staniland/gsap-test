023e8489c864a7ffb050953e9ac1360b910e4101
George Staniland <georgestaniland@gmail.com>
2024-07-10 17:38:05 +1200

    timeline

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
+// Section 1
 
+const timeline = gsap.timeline({ defaults: { duration: 1 } });
+timeline
+  .from('.section1-text', { duration: 1.6, x: '-100vw', ease: 'bounce' } )
+  .from('.section-1 .sequence', { duration: 1, x: '-100vw', stagger: .5, ease: 'bounce'  })
 
+const btn = document.querySelector('.run-btn');
+btn.addEventListener(addEventListener( 'click', () => {
+  timeline.timeScale(3)
+  timeline.reverse();
+} ))
+
+// Section 2
 addEventListener("load", (event) => {
     gsap.registerPlugin(ScrollTrigger) 
 
@@ -46,34 +58,6 @@ addEventListener("load", (event) => {
 });
 
 
-// With TimeLine
-// addEventListener("load", (event) => {
-//   gsap.registerPlugin(ScrollTrigger) 
-
-//   const tl = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: ".section-2",
-//       scrub: true,
-//       start: "top bottom",
-//       end: "top top",
-//       markers: true,
-//     },
-//   })
-//   .to(".section-2 h1", {
-//     x: 0,
-//     transformOrigin: "center center", 
-//     ease: "none",
-//   })
-//   .to(".section-2 .circle", {
-//     height: '90vh',
-//     width: '90vh',
-//     borderColor: 'rgba(0, 0, 0, 1)',
-//     transformOrigin: "center center", 
-//     duration: 1,
-//     ease: "elastic",
-//   }, "-=0.5" )
-
-// });
 
 
 
